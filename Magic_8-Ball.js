56dfa3289940df65408a1720c32f9b400fc5aa0f
AVese2k <31697731+AVese2k@users.noreply.github.com>
2017-10-30 10:11:45 -0400

    Create Magic_8-Ball.js

---
+++ Magic_8-Ball.js
@@ -0,0 +1,18 @@
+fill(0, 0, 0);
+ellipse(200, 200, 375, 375);
+fill(60, 0, 255);
+triangle(200, 104, 280, 280, 120, 280);
+fill(255, 255, 255);
+var answer = floor(random(1, 5));
+if (answer === 1) {
+    text("One", 186, 217); 
+}
+else if (answer === 2) {
+    text("Two", 186, 217);
+}
+else if (answer === 3) {
+    text("Three", 184, 217);
+}
+else if (answer === 4) {
+    text("Four", 186, 217); 
+}
