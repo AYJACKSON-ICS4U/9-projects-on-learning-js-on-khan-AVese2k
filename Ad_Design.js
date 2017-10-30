bae826460c41233cddae462483ef638a3e0c5e74
AVese2k <31697731+AVese2k@users.noreply.github.com>
2017-10-30 10:08:48 -0400

    Create Ad_Design.js

---
+++ Ad_Design.js
@@ -0,0 +1,13 @@
+background(251, 255, 0);
+fill(0, 0, 0);
+rect(140,250,90,10);
+rect(180,220,10,30);
+rect(115,150,140,80);
+fill(94, 88, 88);
+rect(117,152,135,75);
+
+fill(255, 0, 0);
+textSize(30);
+text("THE AMAZING MONITOR ", 19, 100);
+textSize(13);
+text("It doesn't break like the ones at school", 86, 302);
