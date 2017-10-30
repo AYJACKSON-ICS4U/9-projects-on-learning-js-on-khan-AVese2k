47aaf2d38d76d7256ccc2dac6454e163bedaec62
AVese2k <31697731+AVese2k@users.noreply.github.com>
2017-10-30 10:14:23 -0400

    Create Whats_for_Dinner.js

---
+++ Whats_for_Dinner.js
@@ -0,0 +1,36 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300);
+//egg
+ellipse(260,260,100,100);
+fill(255, 247, 0);
+noStroke();
+ellipse(260,260,45,45);
+//bread
+fill(227, 190, 102);
+strokeWeight(5);
+stroke(84, 66, 42);
+rect(80,120,100,110,10);
+//brocolli1
+fill(185, 240, 108);
+noStroke();
+rect(120,280,10,30);
+fill(81, 140, 30);
+ellipse(115,278,15,15);
+ellipse(125,278,15,15);
+ellipse(135,278,15,15);
+ellipse(120,268,15,15);
+ellipse(130,268,15,15);
+//brocolli2
+fill(185, 240, 108);
+rect(170,300,10,30);
+fill(81, 140, 30);
+ellipse(165,298,15,15);
+ellipse(175,298,15,15);
+ellipse(185,298,15,15);
+ellipse(170,288,15,15);
+ellipse(180,288,15,15);
+//pepperette
+fill(150, 76, 76);
+rect(220,90,10,100,10);
+rect(250,90,10,100,10);
